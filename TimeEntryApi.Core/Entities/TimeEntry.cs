35920871c1622e4bc3b7ac3694c79ea02d185309
Tom Vaidyan <tvaidyan@gmail.com>
2018-08-09 22:45:17 -0400

    fix: make entity properties public for appropriate access

--- TimeEntryApi.Core/Entities/TimeEntry.cs
+++ TimeEntryApi.Core/Entities/TimeEntry.cs
@@ -2,13 +2,13 @@ using System;
 
 public class TimeEntry : IEntity
 {
-    int TimeEntryId { get; set; }
-    int ProjectId { get; set; }
-    DateTime EntryDateTime { get; set; }
-    string DetailsForInvoice { get; set; }
-    bool IsBillable {get;set;}
-    string InternalNotes {get;set;}
-    DateTime ReviewedDate {get;set;}
+    public int TimeEntryId { get; set; }
+    public int ProjectId { get; set; }
+    public DateTime EntryDateTime { get; set; }
+    public string DetailsForInvoice { get; set; }
+    public bool IsBillable {get;set;}
+    public string InternalNotes {get;set;}
+    public DateTime ReviewedDate {get;set;}
     public string ReviewedBy { get; set; }
     public DateTime InvoicedDate { get; set; }
     public string InvoicedBy { get; set; }
